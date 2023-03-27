10631e824c8ce4a2de9f03cbf9746a9b26a4acae
Ethan Duggan <eduggan1997@gmail.com>
2023-03-27 16:18:34 -0400

    added mongoDB database and did a bunch of other changes.  This is the code as of the end of part 3d section 'Deploying the database backend to production'

--- index.js
+++ index.js
@@ -1,22 +1,136 @@
+//import environment variables
+require('dotenv').config()
+
+//import modules
 const express = require('express')
 const cors = require('cors')
 const app = express()
 
+//import mongoose models
+const Note = require('./models/note')
+
+
 //custom middleware
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
+    console.log('Method: ', request.method)
     console.log('Path: ', request.path)
     console.log('Body: ', request.body)
     console.log('---')
     next()
 }
 
+//apply middlewares
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
 
-let notes = [
+
+//start server
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
+
+//ROUTES
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (request, response) => {
+    Note.find({}).then(notes => {
+        response.json(notes)
+    })
+})
+
+app.get('/api/notes/:id', (request, response, next) => {
+    Note.findById(request.params.id)
+        .then(note => {
+            if (note) {
+                response.json(note)
+            } else {
+                response.status(404).end()
+            }
+        })
+        .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+    const body = request.body
+
+    const note = new Note({
+        content: body.content,
+        important: body.important || false,
+    })
+
+    note.save()
+        .then(savedNote => {
+            response.json(savedNote)
+        })
+        .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
+})
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const {content, important} = request.body
+
+    Note.findByIdAndUpdate(
+        request.params.id,
+        {content, important},
+        {new: true, runValidators: true, context: 'query'}
+    )
+        .then(updatedNote => {
+            response.json(updatedNote)
+        })
+        .catch(error => next(error))
+})
+
+//helper functions
+
+const generateNoteID = () => {
+    const maxID = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxID + 1
+}
+
+
+//custom middleware for use after routes
+
+const unkownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+app.use(unkownEndpoint)
+
+
+//custom error handler middleware
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+
+    next(error)
+}
+
+app.use(errorHandler) //this has to be the last loaded middleware
+
+
+//TRASH
+
+/*let notes = [
     {
         id: 1,
         content: "HTML is easy",
@@ -32,71 +146,4 @@ let notes = [
         content: "GET and POST are the most important methods of HTTP protocol",
         important: true
     }
-]
-
-app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
-})
-
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const note = notes.find(note => note.id === id)
-
-    if (note) {
-        response.json(note)
-    } else {
-        response.statusMessage = `couldn't find note with id=${id}`
-        response.status(404).end()
-    }
-})
-
-app.post('/api/notes', (request, response) => {
-    const body = request.body
-
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
-
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateNoteID()
-    }
-
-    notes = notes.concat(note)
-
-    response.json(note)
-})
-
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
-
-    response.status(204).end()
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-
-//helper functions
-
-const generateNoteID = () => {
-    const maxID = notes.length > 0
-        ? Math.max(...notes.map(n => n.id))
-        : 0
-    return maxID + 1
-}
-
-//custom middleware for use after routes
-const unkownEndpoint = (request, response) => {
-    response.status(404).send({error: 'unkown endpoint'})
-}
-app.use(unkownEndpoint)
+]*/
