30beef238c49c933deb4473d113d34f732255fe1
Ethan Duggan <eduggan1997@gmail.com>
2023-03-28 11:17:17 -0400

    made changes to the .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@ module.exports = {
 	'env': {
 		'node': true,
 		'commonjs': true,
-		'es2021': true
+		'es2021': true,
 	},
 	'extends': 'eslint:recommended',
 	'overrides': [
@@ -26,6 +26,15 @@ module.exports = {
 		'semi': [
 			'error',
 			'never'
-		]
-	}
+		],
+		'eqeqeq': 'error',
+		'no-trailing-spaces': 'error',
+		'object-curly-spacing': [
+			'error', 'always'
+		],
+		'arrow-spacing': [
+			'error', { 'before': true, 'after': true }
+		],
+		'no-console': 0,
+	},
 }
